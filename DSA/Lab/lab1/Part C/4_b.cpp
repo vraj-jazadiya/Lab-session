293b2568e1e981d4b29f9620d9cd382baf465ba7
vraj-jazadiya <vrajjazadiya@gmail.com>
2025-10-07 06:49:24 -0700

    Delete DSA/Lab directory

--- DSA/Lab/lab1/Part C/4_b.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int arr[] = {10, 20, 30, 40, 50};
-    int length = sizeof(arr) / sizeof(arr[0]);
-    cout << "Length of the array: " << length << endl;
-    return 0;
-}
--- DSA/Lab/lab4/lab4/Q7-code-sinpet.c
+++
@@ -1,5 +0,0 @@
-struct Node {
-    int data;
-    struct Node* prev;
-    struct Node* next;
-};
