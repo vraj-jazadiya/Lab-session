3bf4cfda4077b7d76e264ab8000a9abd5a9f3e44
vraj-jazadiya <vrajjazadiya@gmail.com>
2026-01-09 12:27:09 +0530

    Add files via upload

---
+++ SEM4/oopj/lab_1/lab_1_solution (Experiments)/p1.java
@@ -0,0 +1,8 @@
+public class p1 {
+    public static void main(String args[]) {
+
+        System.out.println();
+        System.out.println("hello how r u?");
+    }
+
+}
---
+++ SEM4/oopj/lab_1/lab_1_solution (Experiments)/p4.java
@@ -0,0 +1,20 @@
+public class p4 {
+
+    public static void main(String args[]) {
+
+        System.out.println("");
+        System.out.println("assignment operator");
+
+        int x = 5, y = 6, z = 5, m = 5;
+        x -= 3;
+        y += 5;
+        z = z * 3;
+        m = m / 1;
+
+        System.out.println("value of x=" + x);
+        System.out.println("value of y=" + y);
+        System.out.println("value of z=" + z);
+        System.out.println("value of m=" + m);
+
+    }
+}
